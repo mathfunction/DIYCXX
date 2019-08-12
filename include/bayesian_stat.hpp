56d7842479bf5a17d0072a6a8d13f4ab99952528
tommy_cheng <tommy_cheng@bb.com>
2019-08-12 12:09:20 +0800

    Monday Lunch

---
+++ include/bayesian_stat.hpp
@@ -0,0 +1,35 @@
+#ifndef __BAYESIAN_STAT_HPP__
+#define __BAYESIAN_STAT_HPP__
+
+
+
+namespace cxxuseful{
+	namespace bayesian_stat{
+
+
+	
+
+
+
+
+
+
+	};
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
--- include/timer.hpp
+++ include/timer.hpp
@@ -16,7 +16,11 @@
 
 namespace cxxuseful{
 	using namespace std;
+	
+	
+	/*
 	class Timer{
+		
 		public:
 			chrono::time_point<chrono::steady_clock> start,end;
 			chrono::duration<float> duration;
@@ -30,8 +34,43 @@ namespace cxxuseful{
 			duration = end-start;
 			cerr << program_name << " tooks " << duration.count()*1000.0f << " ms" << endl;
 		}
+		
+	};*/
 
+	class TimerBaseClocker{
+		public:
+			TimerBaseClocker(){ m_start = -1;}
+			void clear(){ m_start = -1;}
+			bool IsStarted() const {return (m_start != -1);}
+			void Start(){ m_start = clock();}
+			clock_t GetMs(){
+				clock_t now;
+				if(IsStarted()){
+					now = clock();
+					clock_t dt = now - m_start;
+					return dt;
+				}//end_if
+				return 0 ;
+			}//end_GetMs  
+		private:	
+			clock_t m_start;
 	};
+
+	class Timer : public TimerBaseClocker{
+		private:
+			string program_name;
+		public:
+			Timer(string program_name){
+				this->program_name = program_name;
+				Start();	
+			};
+			~Timer(){
+				clock_t t = GetMs();
+				cerr << program_name << " costs " << t << " clicks (" << ((float)t)/CLOCKS_PER_SEC << " s)" << endl;
+			};
+	};
+	
+	
 
 
 };//end_cxxuseful
