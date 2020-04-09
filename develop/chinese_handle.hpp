8b3f4bc9745eba3f977e3ce6fef00d8d69173c03
tommy_cheng <tommy_cheng@bb.com>
2020-04-09 09:15:35 +0800

    test

--- develop/chinese_handle.hpp
+++ develop/chinese_handle.hpp
@@ -6,42 +6,56 @@
 	chcp 950 BIG5 , 	each Chinese-Char need 2-Chars
 
 	
-
+// 參考網址 :   https://blog.csdn.net/jcjc918/article/details/52200478
 
 ===============================================================================*/
-#ifndef CHINESE_HANDLE
-#define CHINESE_HANDLE
+#ifndef __CHINESE_HANDLE_HPP__
+#define __CHINESE_HANDLE_HPP__
 
-#include<iostream>
-#include<string>
+#include <memory>
+#include <string>
 
-using namespace std;
 
-class WideStr{
-	public:
-		wstring ws;
-		string s;
-		int size;
-		WideStr(wstring ws){
-			this->ws = ws;
-		}//end_widestr
-		WideStr(string s){
-			unsigned len = s.size()*2;
-    		setlocale(LC_CTYPE, "");
-    		wchar_t *buffer = new wchar_t[len];
-    		mbstowcs(buffer,s.c_str(),len);
-    		this->ws = wstring(buffer);
-    		delete[] buffer;
-		}//end_widestr
-		void print(){
-			setlocale(LC_CTYPE, "");
-			for(int i=0;i<ws.size();i++){
-				wprintf(L"%c",ws[i]);
+namespace cxxuseful{
+
+	
+	#if defined _WIN32 || defined _WIN64 
+
+	#else
+		
+		
+		void print(const std::wstring& _wstr){
+			for(int i=0;i<_wstr.size();i++){
+				std::wcout << _wstr[i];
 			}//endfor
-		}//end_print	
-};//end_class
+			std::wcout << "\n";
+		}//end_print
+		
+		std::wstring s2ws(const std::string& _str) {
+		  if (_str.empty()) {
+		    return L"";
+		  }//endif
+		  setlocale(LC_CTYPE, "en_US.UTF-8");
+		  unsigned len = _str.size()+1;
+		  std::unique_ptr<wchar_t[]> p(new wchar_t[len]);
+		  mbstowcs(p.get(), _str.c_str(), len);
+		  std::wstring _wstr(p.get());
+		  return _wstr;
+		}
 
+		std::string ws2s(const std::wstring& _wstr) {
+		    if (_wstr.empty()) {
+		      return "";
+		    }//endif
+		    setlocale(LC_CTYPE, "en_US.UTF-8");
+		    unsigned len = _wstr.size()*4 + 1;
+		    std::unique_ptr<char[]> p(new char[len]);
+		    wcstombs(p.get(), _wstr.c_str(), len);
+		    std::string _str(p.get());
+		    return _str;
+		}//endif
+	#endif
 
-
+};
 
 #endif
